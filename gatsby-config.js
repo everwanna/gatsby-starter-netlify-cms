461a1f7fcebd28ac0b158312be793d0142a01921
william <everwanna@users.noreply.github.com>
2023-04-04 16:25:08 -0700

    update site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "AI打怪学院",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "加入AI打怪学院，一起打怪升级，提高AI技能！",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
